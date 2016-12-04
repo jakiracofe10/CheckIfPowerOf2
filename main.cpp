b2d3169bad28e7df36ebe1185d13799a0de43bab
Jason <jakiracofe10@gmail.com>
2016-12-03 19:02:50 -0600

    first

--- main.cpp
+++ main.cpp
@@ -3,20 +3,13 @@
 using namespace std;
 
 bool checkIfPowerOf2(uint64_t n) {
-    if (n<=0) {
-        return false;
-    }
     //turn off right most bit
-    uint64_t x = n & (n-1);
     //if x is 0 then, power of two, because power of two has only one one-bit
-    if (!x) {
-        return true;
-    }
-    return false;
+    return (n > 0 && (n&(n-1))==0);
 }
 
 int main() {
-    uint64_t n = 100;
+    uint64_t n = 10;
     bool result = checkIfPowerOf2(n);
     cout << result << endl;
     return 0;
